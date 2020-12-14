87f23c55a8fff662aac8ce2245d22a53e9fcfd4c
ryosukegym <73817040+ryosukegym@users.noreply.github.com>
2020-12-14 11:39:44 +0900

    ユーザー管理準備２

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
       username == 'admin' && password == '0722'
       # username == ENV["BASIC_AUTH_USER"] && password ==["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name,:last_name,:first_name_katakana,:last_name_katakana,:birthday])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :last_name, pretence: true
+  validates :first_name, pretence: true
+  validates :last_name_katakana, pretence: true
+  validates :first_name_katakana, pretence: true
+  validates :birthday, presence: true
+
+  has_many :items
+  has_many :buyers
+
 end
