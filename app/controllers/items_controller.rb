c0cb0d1af0130029ec2f1d0347502e710c3f004b
ryosukegym <73817040+ryosukegym@users.noreply.github.com>
2020-12-18 20:29:57 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,22 +23,36 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def edit
-  #   @item = Item.find(params[:id])
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
 
-
-
-
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+    # @item.update(item_params)
+    # if user_signed_in? 
+    #   if current_user.id==@item.user.id
+    #     redirect_to edit_item_path
+    #   end
+    # else
+    #   render :index
+    end
+  end
   private
 
   def item_params
     params.require(:item).permit(:title, :explanation, :category_id, :product_condition_id, :delivery_fee_id, :prefectural_id,
                                  :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
-end
 
-def move_to_index
-  unless user_signed_in?
-   redirect_to new_user_session_path
+  def move_to_index
+    unless user_signed_in?
+    redirect_to new_user_session_path
+    end
   end
-end
